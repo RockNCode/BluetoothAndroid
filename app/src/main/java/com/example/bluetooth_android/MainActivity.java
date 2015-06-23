f7fe0b3de4d3925e617876060a3da5c76b4720cb
Manuel Garcia <manuel.gar@samsung.com>
2015-06-22 17:14:39 -0700

    Up to end of video 3.

--- app/src/main/java/com/example/bluetooth_android/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_android/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.bluetooth_android;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -14,20 +17,36 @@ public class MainActivity extends Activity {
     ArrayAdapter<String> listAdapter;
     Button connectNew;
     ListView listView;
+    BluetoothAdapter btAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        if(btAdapter == null){
+            Toast.makeText(getApplicationContext(),"No bluetooth dected",0).show();
+            finish();
+        }else{
+            if(!btAdapter.isEnabled()){
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intent,1);
+            }
+        }
     }
-
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == RESULT_CANCELED){
+            Toast.makeText(getApplicationContext(),"Bluetooth must be enabled to continue",Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
     private void init() {
         connectNew = (Button)findViewById(R.id.bConnectNew);
         listView = (ListView)findViewById(R.id.listView);
         listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,0);
-        String mArray[] = {"Device 1","Device 2","Device 3"};
-        listAdapter.addAll(mArray);
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
         listView.setAdapter(listAdapter);
     }
 
